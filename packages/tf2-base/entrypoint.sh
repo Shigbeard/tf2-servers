53c81f23e49975d01f1efb5dd4a13d9153a48d66
Shigbeard <4262327+Shigbeard@users.noreply.github.com>
2022-12-25 14:06:52 +1100

    additional shit for Summer Brawl 23

--- packages/tf2-base/entrypoint.sh
+++ packages/tf2-base/entrypoint.sh
@@ -44,6 +44,7 @@ faketty $SERVER_DIR/srcds_run \
   +clientport ${CLIENT_PORT} \
   -steamport ${STEAM_PORT} \
   +tv_port ${STV_PORT} \
+  +map ${MAP} \
   ${ADDITIONAL_ARGS} \
   -strictportbind \
   -norestart \
